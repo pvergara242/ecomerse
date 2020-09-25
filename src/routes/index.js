424c5199abef8c7efacd7efc2add97ee409e5734
shecho <shechovala@gmail.com>
2020-09-24 20:11:37 -0500

    simple router added

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+module.exports = app => {
+    // Index or single pages routes
+ 
+    // Error router
+    router.get("/timeout", home.error504);
+    router.get("*", home.error404);
+  
+    app.use(router);
+  };
