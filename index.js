4ed6a10a7f18ccdfbbe962098f80203764ef71f8
shecho <shechovala@gmail.com>
2020-10-02 19:22:32 -0500

    added depdencies Bycrip nodemailer and jwt

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const config = require("./server/config");
 const app = config(express());
 
 app.listen(app.get(`port`), () => {
-  console.log(`Saludo para jos in the por port: ${app.get("port")} las nalgas`);
+  console.log(`Conected on port: ${app.get("port")} `);
 });
-
