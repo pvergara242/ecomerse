130b5f1d14ed4804fa608d9a79bdf9bc151d7d25
shecho <shechovala@gmail.com>
2020-10-03 16:00:19 -0500

    added babel dependencies

--- server/config.js
+++ server/config.js
@@ -3,21 +3,19 @@ const express = require("express");
 const path = require("path");
 const routes = require("../routes/index");
 
-
 module.exports = (app) => {
-    // Configuration
-    app.set("port", process.env.PORT || 3000);
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
-  
-    // Static files
-    app.use("/public", express.static(path.join(__dirname, "../public")));
+  // Configuration
+  app.set("port", process.env.PORT || 3000);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-  
-    // Routes
-    routes(app);
-    return app;
+  // Static files
+  app.use("/public", express.static(path.join(__dirname, "../public")));
+
+  // Routes
+  routes(app);
+  return app;
 
   // Error router
-  };
-  
+};
+
