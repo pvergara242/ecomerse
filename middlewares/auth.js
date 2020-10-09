5356596957959c7defbd8d222e715ff86721a0b8
shecho <shechovala@gmail.com>
2020-10-08 21:26:49 -0500

    addde delete user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,25 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const validateToken = (request, response, next) => {
-    //Middleware para validar el token del usuario
-    // token = request.headers
-    if(request.headers.authorization){
-        const authorization = request.headers.authorization.split(" ");
-        const token = authorization[1];
+  //Middleware para validar el token del usuario
+  // token = request.headers
+  if (request.headers.authorization) {
+    const authorization = request.headers.authorization.split(" ");
+    const token = authorization[1];
 
-        try {
-            let decoded = jwt.verify(token, process.env.JWT_SECRET);
-            if(decoded){
-                next();
-            }
-        } catch(err) {
-            console.log(err);
-            response.status(401).json({message: "El token es invalido"})
-        }
-    }else{
-        response.status(401).json({message: "El token no ha sido proporcionado"})
+    try {
+      request["token"] = token;
+      let decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded) {
+        next();
+      }
+    } catch (err) {
+      console.log(err);
+      response.status(401).json({ message: "El token es invalido" });
     }
-}
+  } else {
+    response.status(401).json({ message: "El token no ha sido proporcionado" });
+  }
+};
 
 module.exports = validateToken;
--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,9 @@ const { Users } = require("../models/index.js");
 const { Model } = require("sequelize");
 const bcrypt = require("bcryptjs");
 
-
 module.exports = (app) => {
-  // home
-  router.get("/", (req, res) => {
-    // res.send("esta el la ruta inicial");
-
-    let clave = process.env.JWT_SECRET || "sergio"
-    console.log(clave)
-    res.json({
-      message: "el clave secreta es:",
-      clave,
-    });
-  });
-
   // user
+  // Create user Register user
   app.post("/api/v1/users", async (request, response) => {
     // let secret = process.env.JWT_SECRET || "orlando"
     // console.log(secret)
@@ -39,7 +27,7 @@ module.exports = (app) => {
       email,
       active,
       token,
-      password:passwordEncrypted,
+      password: passwordEncrypted,
       created_at: new Date(),
       updated_at: new Date(),
     });
@@ -49,91 +37,142 @@ module.exports = (app) => {
       user,
     });
   });
-
-  
-app.post('/api/v1/users/login', async (request,response)=>{
-  const  {email,password} = request.body;
-  let secret = process.env.JWT_SECRET || "orlando"
-  let user = await Users.findOne({where:{email:email}});
-  if(user){
-      bcrypt.compare(password, user.password, function(err,res){
-          if(err || res === false){
-              response.status(401).json({message:"las credenciales son incorrectas"})
-          }else{
-              const token = jwt.sign({id:user.id,email:user.email,password:user.password},secret,{expiresIn:'30m'})
-              response.json({message: "has iniciado sesion correctamente",token:token});
-          }
-      })  
-  }else{
-      response.status(401).json({message:"no existe el usuario con el mismo correo ingresado"})
-  }
-  
-})
-// app.use(validateToken);
-
-app.get('/api/v1/users', async(request, response) => {
-  const users = await Users.findAll();
-  response.json({results: users})
-});
-
-app.get('/api/v1/users/:id', async(request, response) => {
-  const userId = request.params.id;
-  const users = await Users.findOne({
-      where: {
-          id: userId
-      }
+  // login
+  app.post("/api/v1/users/login", async (request, response) => {
+    const { email, password } = request.body;
+    let secret = process.env.JWT_SECRET || "orlando";
+    let user = await Users.findOne({ where: { email: email } });
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (err || res === false) {
+          response
+            .status(401)
+            .json({ message: "las credenciales son incorrectas" });
+        } else {
+          const token = jwt.sign(
+            { id: user.id, email: user.email, password: user.password },
+            secret,
+            { expiresIn: "1h" }
+          );
+          response.json({
+            message: "has iniciado sesion correctamente",
+            token: token,
+          });
+        }
+      });
+    } else {
+      response.status(401).json({
+        message: "no existe el usuario con el mismo correo ingresado",
+      });
+    }
   });
-  response.json(users)
-});
 
+  //  enviar correo nodemeiler route
 
+  // ------------------------------------------------
+  //Token validation middlewares
+  app.use(validateToken);
 
-app.put('/api/v1/users/:id', async(request, response) => {
-  let userId = request.params.id;
-  let {
+  // home
+  router.get("/", (req, res) => {
+    // res.send("esta el la ruta inicial");
+
+    let clave = process.env.JWT_SECRET || "sergio";
+    console.log(clave);
+    res.json({
+      message: "el clave secreta es:",
+      clave,
+    });
+  });
+
+  //  get one user
+  app.get("/api/v1/users/:id", async (request, response) => {
+    const userId = request.params.id;
+    const users = await Users.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    response.json(users);
+  });
+
+  //  Editar  User
+  app.put("/api/v1/users/:id", async (request, response) => {
+    let userId = request.params.id;
+    let {
       first_name,
       last_name,
       email,
       active,
       token,
-      password
-  } = request.body;
-  try {
-      const users = await Users.update({
+      password,
+    } = request.body;
+    try {
+      const users = await Users.update(
+        {
           first_name,
           last_name,
           email,
           active,
           token,
           password,
-          updated_at: new Date()
-      }, {
+          updated_at: new Date(),
+        },
+        {
           returning: true,
           where: {
-              id: userId
-          }
-      });
+            id: userId,
+          },
+        }
+      );
       const user = users[1][0].dataValues;
       response.json(user);
-  } catch (error) {
+    } catch (error) {
       response
-          .status(400)
-          .json({message: "No se ha podido actualizar el registro"});
-  }
-});
-
-app.delete('/api/v1/users/:id', async(request, response) => {
-  let userId = request.params.id;
-  let user = await Users.destroy({
-      where: {
-          id: userId
-      }
+        .status(400)
+        .json({ message: "No se ha podido actualizar el registro" });
+    }
   });
-  response.json({message: "El registro se ha eliminado correctamente", user});
-});
 
+  // List all users
+  app.get("/api/v1/users", async (request, response) => {
+    const users = await Users.findAll();
+    response.json({ results: users });
+  });
 
+  // ELiminar user
+  app.delete("/api/v1/users/:id", async (request, response) => {
+    let userId = request.params.id;
+    let decoded = jwt.verify(request.token, process.env.JWT_SECRET);
+    try {
+      let user = await Users.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      if (decoded.id !== Number(userId) && user) {
+        await Users.update(
+          { active: false },
+          {
+            where: {
+              id: userId,
+            },
+          }
+        );
+        response.json({ message: "La cuenta ha sido desactivada" });
+      } else {
+        response
+          .status(400)
+          .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
+      }
+    } catch (error) {
+      response
+        .status(400)
+        .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
+    }
+  });
 
+  // rest pasword
   router.post("/api/v1/users/reset-password", (req, res) => {
     console.log(req);
     res.json({
@@ -149,8 +188,7 @@ app.delete('/api/v1/users/:id', async(request, response) => {
       message: "something goes wrong on update pasword",
     });
   });
-  
-  
+
   // router.get("/timeout", home.error504);
   // router.get("*", home.error404);
 
