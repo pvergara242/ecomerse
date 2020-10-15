194050499edc5c1f45414c31b2dbd228f380701a
shecho <shechovala@gmail.com>
2020-10-14 22:43:44 -0500

    some trimp on white spaces

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import config from './server/config';
 // const config = require("./server/config");
 
 const app = config(express());
-
 app.listen(app.get(`port`), () => {
   console.log(`Conected on port: ${app.get("port")} `);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 import {Router} from 'express'
 import validateToken  from "../middlewares/auth";
 
-
 // const express = require("express");
 // import bcrypt from 'bcryptjs'
-
-
 const router = Router();
-
 // const validateToken = require("../middlewares/auth");
 // const jwt = require("jsonwebtoken");
 // const { Users } = require("../models/index.js");
@@ -15,7 +11,6 @@ const router = Router();
 // const { Model } = require("sequelize");
 // const enviarCorreo = require("../middlewares/nodemailer");
 // const user = require("../controllers/users");
-
 // Middlewares
 import {Model} from "sequelize"
 import enviarCorreo from "../middlewares/nodemailer";
@@ -44,18 +39,14 @@ module.exports = (app) => {
     });
   });
 // user
-
   // Create user Register user
   app.post("/api/v1/users", user.register);
   // login
   app.post("/api/v1/users/login", user.login);
-
   // resetear user pasword
   app.post("/api/v1/users/reset-password", user.reset);
   // update-password
   router.post("/api/v1/users/update-password", user.update);
-
-
   //Token validation middlewares
   app.use(validateToken);
   //  get one user
@@ -70,6 +61,5 @@ module.exports = (app) => {
   router.post("/api/v1/users/reset-password", user.reset);
   // router.get("/timeout", home.error504);
   // router.get("*", home.error404);
-
   app.use(router);
 };
