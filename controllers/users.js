f49fe7d8ca24f07d974429be9aa2119c55e6948f
shecho <shechovala@gmail.com>
2020-10-14 20:16:10 -0500

    added user controller function

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,196 @@
+const bcrypt = require("bcryptjs");
 
-routes.get('/')
+const resgister = async (request, response) => {
+  // let secret = process.env.JWT_SECRET || "orlando"
+  // console.log(secret)
+  let { first_name, last_name, email, active, token, password } = request.body;
+  const passwordEncrypted = bcrypt.hashSync(password, 10);
+  const user = await Users.create({
+    first_name,
+    last_name,
+    email,
+    active,
+    token,
+    password: passwordEncrypted,
+    created_at: new Date(),
+    updated_at: new Date(),
+  });
+  console.log(user);
+  response.json({
+    message: "Se ha agregado el usuario satisfactoriamente",
+    user,
+  });
+};
+// Login user
+const login = async (request, response) => {
+  const { email, password } = request.body;
+  let secret = process.env.JWT_SECRET || "orlando";
+  let user = await Users.findOne({ where: { email: email } });
+  if (user) {
+    bcrypt.compare(password, user.password, function (err, res) {
+      if (err || res === false) {
+        response
+          .status(401)
+          .json({ message: "las credenciales son incorrectas" });
+      } else {
+        const token = jwt.sign(
+          { id: user.id, email: user.email, password: user.password },
+          secret,
+          { expiresIn: "1h" }
+        );
+        response.cookie("access_token", "Bearer " + token, {
+          expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
+        });
+        response.json({
+          message: "has iniciado sesion correctamente",
+          token: token,
+        });
+      }
+    });
+  } else {
+    response.status(401).json({
+      message: "no existe el usuario con el mismo correo ingresado",
+    });
+  }
+};
+// update user
+const update = (request, response) => {};
+
+// send email
+const reset = async (request, response) => {
+  // console.log(req.body);
+  try {
+    const { email } = req.body;
+    let user = await Users.findOne({ where: { email: email } });
+    if (user) {
+      console.log(Users);
+    } else {
+      console.log("No se han encontrado nada");
+    }
+    const emailMaster = process.env.GOOGLE_ACCOUNT || "pvergara242@gmail.com";
+    const objeto = {
+      from: emailMaster,
+      to: email,
+      subject: "Restaurar contraseña",
+      text: `Su correo es:${email}`,
+    };
+    enviarCorreo(objeto);
+    res.json({
+      data: user.email,
+      message: "el correo se ha enviado satisfactoriamente",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// update password
+const update = (request, response) => {
+  res.json({
+    data: {},
+    message: "something goes wrong on update pasword",
+  });
+};
+
+// logout
+const logout = (request, response) => {
+  res.json({
+    data: {},
+    message: "you have logout the session",
+  });
+};
+
+// reset pasword
+const reset = (request, response) => {
+  console.log(req);
+  res.json({
+    data: {},
+    message: "something goes wrong in password",
+  });
+};
+
+// delete
+const deleteUser = async(request, response) => {
+    let decoded = jwt.verify(request.token, process.env.JWT_SECRET);
+    try {
+      let user = await Users.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      if (decoded.id !== Number(userId) && user) {
+        await Users.update(
+          { active: false },
+          {
+            where: {
+              id: userId,
+            },
+          }
+        );
+        response.json({ message: "La cuenta ha sido desactivada" });
+      } else {
+        response
+          .status(400)
+          .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
+      }
+    } catch (error) {
+      response
+        .status(400)
+        .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
+    }
+};
+
+// list all ussers
+
+const listAll = (request,response) => {
+    const users = await Users.findAll();
+    response.json({ results: users });
+}
+
+// get one user
+const getUser = async(request,response) => {
+    const userId = request.params.id;
+    const users = await Users.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    response.json(users);
+}
+
+//  Edit  User
+const edit = async(request,response) => {
+    let userId = request.params.id;
+    let {
+      first_name,
+      last_name,
+      email,
+      active,
+      token,
+      password,
+    } = request.body;
+    try {
+      const users = await Users.update(
+        {
+          first_name,
+          last_name,
+          email,
+          active,
+          token,
+          password,
+          updated_at: new Date(),
+        },
+        {
+          returning: true,
+          where: {
+            id: userId,
+          },
+        }
+      );
+      const user = users[1][0].dataValues;
+      response.json(user);
+    } catch (error) {
+      response
+        .status(400)
+        .json({ message: "No se ha podido actualizar el registro" });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,121 +5,33 @@ const validateToken = require("../middlewares/auth");
 const { Users } = require("../models/index.js");
 const { Model } = require("sequelize");
 const bcrypt = require("bcryptjs");
-const enviarCorreo = require('../middlewares/nodemailer');
-
+const enviarCorreo = require("../middlewares/nodemailer");
 
 module.exports = (app) => {
   // user
   // Create user Register user
-  app.post("/api/v1/users", async (request, response) => {
-    // let secret = process.env.JWT_SECRET || "orlando"
-    // console.log(secret)
-    let {
-      first_name,
-      last_name,
-      email,
-      active,
-      token,
-      password,
-    } = request.body;
-    const passwordEncrypted = bcrypt.hashSync(password, 10);
-    const user = await Users.create({
-      first_name,
-      last_name,
-      email,
-      active,
-      token,
-      password: passwordEncrypted,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
-    console.log(user);
-    response.json({
-      message: "Se ha agregado el usuario satisfactoriamente",
-      user,
-    });
-  });
+  app.post("/api/v1/users", async (request, response) => {});
   // login
-  app.post("/api/v1/users/login", async (request, response) => {
-    const { email, password } = request.body;
-    let secret = process.env.JWT_SECRET || "orlando";
-    let user = await Users.findOne({ where: { email: email } });
-    if (user) {
-      bcrypt.compare(password, user.password, function (err, res) {
-        if (err || res === false) {
-          response
-            .status(401)
-            .json({ message: "las credenciales son incorrectas" });
-        } else {
-          const token = jwt.sign(
-            { id: user.id, email: user.email, password: user.password },
-            secret,
-            { expiresIn: "1h" }
-          );
-          response.cookie('access_token', 'Bearer ' + token, {
-            expires: new Date(Date.now() + 8 * 3600000) // cookie will be removed after 8 hours
-        })
-          response.json({
-            message: "has iniciado sesion correctamente",
-            token: token,
-          });
-        }
-      });
-    } else {
-      response.status(401).json({
-        message: "no existe el usuario con el mismo correo ingresado",
-      });
-    }
-  });
+  app.post("/api/v1/users/login", async (request, response) => {});
 
-  app.post('/api/v1/enviar-correo',(req,res)=>{
+  app.post("/api/v1/enviar-correo", (req, res) => {
     enviarCorreo();
     res.json({
-      message: 'el correo se ha enviado satisfactoriamente'
-    })
-  })
-  
-  router.post('/api/v1/users/reset-password', async(req, res) => {
-    // console.log(req.body);
-    try {
-      const { email } = req.body;
-      let user = await Users.findOne({ where: { email: email } });
-      if (user) {
-        console.log(Users)
-      } else {
-        console.log('No se han encontrado nada')
-      }
-      const emailMaster = process.env.GOOGLE_ACCOUNT || 'pvergara242@gmail.com';
-    const objeto = {
-      from: emailMaster,
-      to: email,
-      subject: 'Restaurar contraseña',
-      text: `Su correo es:${email}` 
-  }
-      enviarCorreo(objeto);
-      res.json({
-        data: user.email,
-        message: 'el correo se ha enviado satisfactoriamente'
+      message: "el correo se ha enviado satisfactoriamente",
     });
-  } catch (error) {
-    console.log(error)
-  }
-});
-
-// .POST → api/v1/users/update-password
-router.post('/api/v1/users/update-password', (req, res) => {
-  console.log(req);
-  res.json({
-    data: {},
-    message: 'something goes wrong on update pasword',
   });
-});
+
+  // resetear user pasword
+  app.post("/api/v1/users/reset-password", async (req, res) => {});
+
+  // update-password
+  router.post("/api/v1/users/update-password", (req, res) => {});
 
   // ------------------------------------------------
   //Token validation middlewares
   app.use(validateToken);
 
-  // home
+  // home view
   router.get("/", (req, res) => {
     // res.send("esta el la ruta inicial");
 
@@ -132,108 +44,19 @@ router.post('/api/v1/users/update-password', (req, res) => {
   });
 
   //  get one user
-  app.get("/api/v1/users/:id", async (request, response) => {
-    const userId = request.params.id;
-    const users = await Users.findOne({
-      where: {
-        id: userId,
-      },
-    });
-    response.json(users);
-  });
+  app.get("/api/v1/users/:id", async (request, response) => {});
 
   //  Editar  User
-  app.put("/api/v1/users/:id", async (request, response) => {
-    let userId = request.params.id;
-    let {
-      first_name,
-      last_name,
-      email,
-      active,
-      token,
-      password,
-    } = request.body;
-    try {
-      const users = await Users.update(
-        {
-          first_name,
-          last_name,
-          email,
-          active,
-          token,
-          password,
-          updated_at: new Date(),
-        },
-        {
-          returning: true,
-          where: {
-            id: userId,
-          },
-        }
-      );
-      const user = users[1][0].dataValues;
-      response.json(user);
-    } catch (error) {
-      response
-        .status(400)
-        .json({ message: "No se ha podido actualizar el registro" });
-    }
-  });
+  app.put("/api/v1/users/:id", async (request, response) => {});
 
   // List all users
-  app.get("/api/v1/users", async (request, response) => {
-    const users = await Users.findAll();
-    response.json({ results: users });
-  });
+  app.get("/api/v1/users", async (request, response) => {});
 
   // ELiminar user
-  app.delete("/api/v1/users/:id", async (request, response) => {
-    let userId = request.params.id;
-    let decoded = jwt.verify(request.token, process.env.JWT_SECRET);
-    try {
-      let user = await Users.findOne({
-        where: {
-          id: userId,
-        },
-      });
-      if (decoded.id !== Number(userId) && user) {
-        await Users.update(
-          { active: false },
-          {
-            where: {
-              id: userId,
-            },
-          }
-        );
-        response.json({ message: "La cuenta ha sido desactivada" });
-      } else {
-        response
-          .status(400)
-          .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
-      }
-    } catch (error) {
-      response
-        .status(400)
-        .json({ message: "Hubo un error al tratar de desactivar la cuenta" });
-    }
-  });
+  app.delete("/api/v1/users/:id", async (request, response) => {});
 
-  // rest pasword
-  router.post("/api/v1/users/reset-password", (req, res) => {
-    console.log(req);
-    res.json({
-      data: {},
-      message: "something goes wrong in password",
-    });
-  });
-  // .POST → api/v1/users/update-password
-  router.post("/api/v1/users/update-password", (req, res) => {
-    console.log(req);
-    res.json({
-      data: {},
-      message: "something goes wrong on update pasword",
-    });
-  });
+  // reset pasword
+  router.post("/api/v1/users/reset-password", (req, res) => {});
 
   // router.get("/timeout", home.error504);
   // router.get("*", home.error404);
